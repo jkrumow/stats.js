d348170c8f2c08766dbf974b9a056fa06d8876ac
Julian Krumow <julian.krumow@artcom.de>
2020-12-17 23:39:49 +0100

    Fix layout bug.

--- build/stats.js
+++ build/stats.js
@@ -13,9 +13,9 @@
 		var mode = 0;
 
 		var container = document.createElement( 'div' );
+		container.style.cssText = 'position:fixed;top:0;left:0;cursor:pointer;opacity:0.9;z-index:10000';
 
 		if (!wideLayout) {
-			container.style.cssText = 'position:fixed;top:0;left:0;cursor:pointer;opacity:0.9;z-index:10000';
 			container.addEventListener( 'click', function ( event ) {
 
 				event.preventDefault();
